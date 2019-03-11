ccdde92b7dfdb9de1498093075c2120c0177c44a
David Humphreys <david.humphreys@mlmgroup.com>
2019-03-11 09:21:16 +0000

    Updated to new version

--- MahApp.Extra/Properties/AssemblyInfo.cs
+++ MahApp.Extra/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows.Markup;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MahApps.Extra")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,8 +49,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.6.5.0")]
+[assembly: AssemblyFileVersion("1.6.5.0")]
 
 [assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/extra", "MahApps.Extra")]
 [assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/extra", "MahApps.Extra.Controls")]
